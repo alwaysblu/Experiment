66eed3b697b25d973c9d5a68a3bfa7953bda7d1d
alwaysblu <stepsw112@gmail.com>
2022-07-16 09:34:05 +0900

    Add project

---
+++ Experiment/Experiment/ExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExperimentApp.swift
+//  Experiment
+//
+//  Created by 최정민 on 2022/07/16.
+//
+
+import SwiftUI
+
+@main
+struct ExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
